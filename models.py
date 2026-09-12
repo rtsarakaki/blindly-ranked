26be7e719f78617a79b0cc960cefa1e5d3b70a67
rtsarakaki <rtsarakaki@gmail.com>
2026-09-12 13:27:51 -0300

    chore: remove arquivos desnecessários

--- models.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class Element(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    category: str = Field(index=True)
-    name: str = Field(index=True)
--- utils.py
+++
@@ -1,4 +0,0 @@
-from sqlmodel import SQLModel, Session
-
-def create_db_and_tables(engine):
-    SQLModel.metadata.create_all(engine)
